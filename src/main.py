ef6d9c2a7e732e2cc2956e63778e9b2fb1b41ba7
fabianradenta <13522105@mahasiswa.itb.ac.id>
2024-03-19 09:23:01 +0700

    add comment on main

--- src/main.py
+++ src/main.py
@@ -6,23 +6,31 @@ import time
 
 utils.welcome()
 try : 
+
+    # terima input jumlah iterasi dan titik kontrol dari user
     iterasi = int(input("Jumlah iterasi : "))
     input_point_1 = input("Titik 1 : ")
     input_point_2 = input("Titik 2 : ")
     input_point_3 = input("Titik 3 : ")
 
+    # processing input dari string menjadi point
     p1, p2, p3 = utils.process_input_point(input_point_1, input_point_2, input_point_3)
 
+    # memeriksa apakah ada kesalahan dalam input
     if p1 is None or p2 is None or p3 is None:
         raise ValueError("Titik yang dimasukkan tidak valid.")
 
-    hasil = []
+    # terima input metode algoritma
     utils.method_option()
     tipe = int(input("Masukkan pilihan anda : "))
+
+    # periksa apakah input sudah benar
     while tipe not in [1,2] :
         tipe = int(input("Tolong masukkan input yang valid\nMasukkan pilihan anda : "))
 
+    # program utama
     start_time = time.time()
+    hasil = []
     if (tipe==1) :
         hasil = dnc.divide_and_conquer(p1, p2, p3, iterasi)
         hasil.append(p3)
@@ -33,7 +41,7 @@ try :
     execution_time = end_time-start_time
     print("Waktu eksekusi program :", execution_time, "seconds")
 
-    # DRAW CURVE
+    # menggambar kurva
     x_points = [point.x for point in hasil]
     y_points = [point.y for point in hasil]
     plot.figure(figsize=(8, 6))
